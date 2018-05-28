9cad87d7d8f415e068ac7a701f0fafe8820623ce
Frank <fabu@fblz.de>
2018-05-28 23:31:18 +0200

    Use GetValue for registry

--- Modules/SteamTools/SteamTools.psm1
+++ Modules/SteamTools/SteamTools.psm1
@@ -1,43 +1,43 @@
 Function ConvertFrom-VDF {
-    <# 
- .Synopsis 
-     Reads a Valve Data File (VDF) formatted string into a custom object.
+<# 
+.Synopsis 
+    Reads a Valve Data File (VDF) formatted string into a custom object.
 
- .Description 
-     The ConvertFrom-VDF cmdlet converts a VDF-formatted string to a custom object (PSCustomObject) that has a property for each field in the VDF string. VDF is used as a textual data format for Valve software applications, such as Steam.
+.Description 
+    The ConvertFrom-VDF cmdlet converts a VDF-formatted string to a custom object (PSCustomObject) that has a property for each field in the VDF string. VDF is used as a textual data format for Valve software applications, such as Steam.
 
- .Parameter InputObject
-     Specifies the VDF strings to convert to PSObjects. Enter a variable that contains the string, or type a command or expression that gets the string. 
+.Parameter InputObject
+    Specifies the VDF strings to convert to PSObjects. Enter a variable that contains the string, or type a command or expression that gets the string. 
 
- .Example 
-     $vdf = ConvertFrom-VDF -InputObject (Get-Content ".\SharedConfig.vdf")
+.Example 
+    $vdf = ConvertFrom-VDF -InputObject (Get-Content ".\SharedConfig.vdf")
 
-     Description 
-     ----------- 
-     Gets the content of a VDF file named "SharedConfig.vdf" in the current location and converts it to a PSObject named $vdf
+    Description 
+    ----------- 
+    Gets the content of a VDF file named "SharedConfig.vdf" in the current location and converts it to a PSObject named $vdf
 
- .Inputs 
-     System.String
+.Inputs 
+    System.String
 
- .Outputs 
-     PSCustomObject
+.Outputs 
+    PSCustomObject
 
- 
- #>
+
+#>
     param
     (
-		[Parameter(Position=0, Mandatory=$true)]
-		[ValidateNotNullOrEmpty()]
+        [Parameter(Position=0, Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
         [String[]]
         $InputObject
-	)
+    )
 
-        $root = New-Object -TypeName PSObject
-        $chain = [ordered]@{}
-        $depth = 0
-        $parent = $root
-        $element = $null
-		
+    $root = New-Object -TypeName PSObject
+    $chain = [ordered]@{}
+    $depth = 0
+    $parent = $root
+    $element = $null
+
     #Magic PowerShell Switch Enumrates Arrays
     switch -Regex ($InputObject) {
         #Case: ValueKey
@@ -47,85 +47,85 @@ Function ConvertFrom-VDF {
         }
         #Case: ParentKey
         '^\t*"(\S+)"$' { 
-                $element = New-Object -TypeName PSObject
+            $element = New-Object -TypeName PSObject
             Add-Member -InputObject $parent -MemberType NoteProperty -Name $Matches[1] -Value $element
             continue
-            }
+        }
         #Case: Opening ParentKey Scope
         '^\t*{$' {
             $parent = $element
-                $chain.Add($depth, $element)
-                $depth++
+            $chain.Add($depth, $element)
+            $depth++
             continue
-            }
+        }
         #Case: Closing ParentKey Scope
         '^\t*}$' {
-                $depth--
-                $parent = $chain.($depth - 1)
-				$element = $parent
-                $chain.Remove($depth)
+            $depth--
+            $parent = $chain.($depth - 1)
+            $element = $parent
+            $chain.Remove($depth)
             continue
-            }
+        }
         #Case: Comments or unsupported lines
         Default {
             Write-Debug "Ignored line: $_"
             continue
-            }
         }
-
-        return $root
     }
-    
+
+    return $root
+}
+
 Function ConvertTo-VDF
 {
-    <# 
- .Synopsis 
-     Converts a custom object into a Valve Data File (VDF) formatted string.
+<# 
+.Synopsis 
+    Converts a custom object into a Valve Data File (VDF) formatted string.
 
- .Description 
-     The ConvertTo-VDF cmdlet converts any object to a string in Valve Data File (VDF) format. The properties are converted to field names, the field values are converted to property values, and the methods are removed.
+.Description 
+    The ConvertTo-VDF cmdlet converts any object to a string in Valve Data File (VDF) format. The properties are converted to field names, the field values are converted to property values, and the methods are removed.
 
- .Parameter InputObject
-     Specifies PSObject to be converted into VDF strings.  Enter a variable that contains the object. You can also pipe an object to ConvertTo-Json.
+.Parameter InputObject
+    Specifies PSObject to be converted into VDF strings.  Enter a variable that contains the object. You can also pipe an object to ConvertTo-Json.
 
- .Example 
-     ConvertTo-VDF -InputObject $VDFObject | Out-File ".\SharedConfig.vdf"
+.Example 
+    ConvertTo-VDF -InputObject $VDFObject | Out-File ".\SharedConfig.vdf"
 
-     Description 
-     ----------- 
-     Converts the PS object to VDF format and pipes it into "SharedConfig.vdf" in the current directory
+    Description 
+    ----------- 
+    Converts the PS object to VDF format and pipes it into "SharedConfig.vdf" in the current directory
 
- .Inputs 
-     PSCustomObject
+.Inputs 
+    PSCustomObject
 
- .Outputs 
-     System.String
+.Outputs 
+    System.String
 
- 
- #>
+
+#>
     param
     (
         [Parameter(Position=0, Mandatory=$true)]
-		[ValidateNotNullOrEmpty()]
+        [ValidateNotNullOrEmpty()]
         [PSObject]
         $InputObject,
 
         [Parameter(Position=1, Mandatory=$false)]
         [int]
         $Depth = 0
-	)
+    )
     $output = [string]::Empty
-        
+    
     foreach ( $property in ($InputObject.psobject.Properties) ) {
         switch ($property.TypeNameOfValue) {
             "System.String" { 
                 $output += ("`t" * $Depth) + "`"" + $property.Name + "`"`t`t`"" + $property.Value + "`"`n"
                 break
-        }
+            }
             "System.Management.Automation.PSCustomObject" {
                 $element = $property.Value
-            $output += ("`t" * $Depth) + "`"" + $property.Name + "`"`n"
-            $output += ("`t" * $Depth) + "{`n"
+                $output += ("`t" * $Depth) + "`"" + $property.Name + "`"`n"
+                $output += ("`t" * $Depth) + "{`n"
                 $output += ConvertTo-VDF -InputObject $element -Depth ($Depth + 1)
                 $output += ("`t" * $Depth) + "}`n"
                 break
@@ -134,14 +134,14 @@ Function ConvertTo-VDF
                 Write-Error ("Unsupported Property of type {0}" -f $_) -ErrorAction Stop
                 break
             }
-            }
         }
-    
-        return $output
     }
-    
+
+    return $output
+}
+
 Function Get-SteamPath {
-	return "$((Get-ItemProperty HKCU:\Software\Valve\Steam\).SteamPath)".Replace('/','\')
+	return (Get-Item HKCU:\Software\Valve\Steam\).GetValue("SteamPath").Replace("/","\")
 }
 
 Function Get-SteamID64 {
